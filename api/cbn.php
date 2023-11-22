c44e0da5e02626a740eba1c444c02fb15aa74136
afiooo <149303909+afiooo@users.noreply.github.com>
2023-11-22 21:20:07 +0800

    Update cbn.php

--- api/cbn.php
+++ api/cbn.php
@@ -1,80 +1,21 @@
 <?php
-$cityId = '5A';
-$playId = $_GET['id'];
-
-if (empty($playId)) {
-  die('Missing playId parameter');
-}
-
-$relativeId = $playId;
-$type = '1';
-$appId = "kdds-chongqingdemo";
-$url = 'http://portal.centre.bo.cbnbn.cn/others/common/playUrlNoAuth?cityId=5A&playId=' . $playId . '&relativeId=' . $relativeId . '&type=1';
-
-// Generate the sign
+$playId = $_GET['id']?? 'cctv1HD';
 $timestamps = round(microtime(true) * 1000);
-$sign = md5('aIErXY1rYjSpjQs7pq2Gp5P8k2W7P^Y@appId' . $appId . "cityId" . $cityId . "playId" . $playId . "relativeId" . $relativeId . "timestamps" . $timestamps . "type" . $type);
-
-// Initialize cURL
-$curl = curl_init();
-
-// Set cURL options
-curl_setopt_array($curl, array(
-  CURLOPT_URL => $url,
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => '',
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 0,
-  CURLOPT_FOLLOWLOCATION => true,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => 'GET',
-  CURLOPT_HTTPHEADER => array(
-    'appId: kdds-chongqingdemo',
-    'sign: ' . $sign,
-    'timestamps:' . $timestamps,
-    'Content-Type: application/json;charset=utf-8'
-  ),
-));
-
-// Execute cURL request
-$response = curl_exec($curl);
-
-// Check for cURL errors
-if ($response === false) {
-  die('cURL error: ' . curl_error($curl));
-}
-
-// Close cURL
-curl_close($curl);
-
-// Decode the response
-$responseData = json_decode($response);
-
-// Check if the response contains the expected data
-if (isset($responseData->data->result->protocol[0]->transcode[0]->url)) {
-  $videoUrl = $responseData->data->result->protocol[0]->transcode[0]->url;
-  header('Location: ' . $videoUrl);
-  exit;
-} else {
-  die('Unable to retrieve video URL');
-}
-<?php
-$Url = "YOUR_BASE_URL_HERE"; // 请将 YOUR_BASE_URL_HERE 替换为实际的基础 URL
-
-$channelUrls = array(
-    "cctv1" => "$Url?sid=cctv1&s=a48k&Cf.m3u8",
-    "cctv2" => "$Url?sid=cctv2&s=a48k&Cf.m3u8",
-    "cctv3" => "$Url?sid=cctv3&s=a48k&Cf.m3u8",
-    "cctv4" => "$Url?sid=cctv4&s=a48k&Cf.m3u8",
-    "cctv5" => "$Url?sid=cctv5&s=a48k&Cf.m3u8",
-    // ... 其他频道及链接
-    "求索纪录" => "$Url?sid=qsjl&s=a48k_zho&Cf.m3u8"
-);
-
-// 输出频道及链接
-foreach ($channelUrls as $channel => $url) {
-    echo "$channel, $url\n";
-}
-?>
-
+$sign = md5("aIErXY1rYjSpjQs7pq2Gp5P8k2W7P^Y@appIdkdds-chongqingdemocityId5A" . "playId" . $playId . "relativeId" . $playId . "timestamps" . $timestamps . "type1");
+$headers = [
+    "appId: kdds-chongqingdemo",
+    "sign: $sign",
+    "timestamps: $timestamps",
+    "Content-Type: application/json;charset=utf-8"
+];
+$ch = curl_init("http://portal.centre.bo.cbnbn.cn/others/common/playUrlNoAuth?cityId=5A&playId=$playId&relativeId=$playId&type=1");
+curl_setopt_array($ch, [
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTPHEADER => $headers
+]);
+$response = curl_exec($ch);
+curl_close($ch);
+$playurl = json_decode($response)->data->result->protocol[0]->transcode[0]->url;
+header("Location: $playurl");
 ?>
